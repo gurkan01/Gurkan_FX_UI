88d9313b526257e0f39279c2f40f534ff8ac9bbb
gurkan <gcorum01@gmail.com>
2026-08-18 21:31:31 +0300

    UI last update

---
+++ Assets/Scripts/UIFX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIFX : MonoBehaviour
+{
+    [SerializeField] private RectTransform targetImage;
+    [SerializeField] private RectTransform fx;
+
+    private void Start()
+    {
+        // FX'i Image'ın üzerine taşı
+        fx.SetParent(targetImage.parent);
+
+        // Image'ın pozisyonuna getir
+        fx.position = targetImage.position;
+
+        // UI sıralamasında en üste al
+        fx.SetAsLastSibling();
+    }
+}
